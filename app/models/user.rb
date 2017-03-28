74b2b3a438082c740d708c803f01c0dcc9041df7
marc bacani <marcctbacani@gmail.com>
2017-03-28 12:08:20 +0800

    email login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
+         # :omniauthable, omniauth_providers: [:facebook]
   has_many :invitations
 end
