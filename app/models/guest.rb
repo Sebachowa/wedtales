e66a858d93f01f022db51226a395a2c8e9ff71ac
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-28 11:15:41 +0800

    add models, validations, associations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,7 @@
 class Guest < ApplicationRecord
   belongs_to :invitation
+
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :plus, default: 0, presence: true
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,20 @@
 class Invitation < ApplicationRecord
   belongs_to :user
   belongs_to :template
+
+  validates :groom_name, presence: true
+  validates :groom_bio, presence: true
+  validates :bride_name, presence: true
+  validates :bride_bio, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :wedding_description, presence: true
+  validates :rsvp, default: false, presence: true
+  validates :og_title, presence: true
+  validates :og_description, presence: true
+
+
 end
+
+
+
