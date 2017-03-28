a1364bbd96ec251e61967abd58a6ae061bf6f5b2
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-03-28 17:51:01 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'pages#dashboard'
   resources :templates
   resources :invitations do
     resources :guests
