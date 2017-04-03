ce3dabed3f969606e4564a1f43a84263e53a1de4
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-04-03 13:08:37 +0800

    fix the precompilation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require swiper.jquery.min
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
